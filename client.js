b45271d74f313acf89a3628b86e6339ad0c817b4
julysNICK <julystube@gmail.com>
2022-05-29 19:05:12 -0400

    first commit

---
+++ client.js
@@ -0,0 +1,104 @@
+var Client = require('ftp');
+var readlineSync = require('readline-sync');
+var c = new Client();
+
+function commands(c, command) {
+  switch (command) {
+    case 'mkdir':
+      var dir = readlineSync.question('Digite o nome do diretorio: ');
+      c.mkdir(dir, function (err) {
+        if (err) throw err;
+        console.log("criou");
+        var isBack = readlineSync.question('Deseja voltar? (s/n) ');
+        if (isBack === 'y') {
+          commands(c, 'voltar');
+        } else {
+          commands(c, 'sair');
+          c.end();
+        }
+      }
+      )
+      break;
+    case "rmdir":
+      var dir = readlineSync.question('Digite o nome do diretorio: ');
+      c.rmdir(dir, function (err) {
+        if (err) throw console.log(err);
+        console.log("removido");
+        var isBack = readlineSync.question('Deseja voltar? (s/n) ');
+        if (isBack === 'y') {
+          commands(c, 'voltar');
+        } else {
+          commands(c, 'sair');
+          c.end();
+        }
+      }
+      )
+      break;
+    case "pwd":
+      c.pwd(function (err, data) {
+        if (err) throw console.log(err);
+        console.log(data);
+        var isBack = readlineSync.question('Deseja voltar? (s/n) ');
+        if (isBack === 'y') {
+          commands(c, 'voltar');
+        } else {
+          commands(c, 'sair');
+          c.end();
+        }
+      }
+      )
+      break;
+    case "get":
+      var file = readlineSync.question('Digite o nome do arquivo: ');
+      c.get(file, function (err, stream) {
+        if (err) throw console.log(err);
+        stream.once('close', function () {
+          console.log('Transfer complete');
+          var isBack = readlineSync.question('Deseja voltar? (s/n) ');
+          if (isBack === 'y') {
+            commands(c, 'voltar');
+          } else {
+            commands(c, 'sair');
+            c.end();
+          }
+        }
+        )
+      }
+      )
+      break;
+
+    case "put":
+      var file = readlineSync.question('Digite o nome do arquivo: ');
+      c.put('remote/teste/foo.txt', 'remote/teste/foo.remote-copy.txt', function (err) {
+        if (err) throw console.log(
+          err
+        );
+        c.end();
+      });
+      break;
+    default:
+      console.log("comando não encontrado");
+      c.end();
+      break;
+  }
+}
+
+
+
+
+c.on('ready', function (
+) {
+
+
+  var typeCommand = readlineSync.question('type command: ');
+  commands(c, typeCommand);
+
+
+
+});
+// connect to localhost:21 as anonymous
+c.connect({
+
+  user: "anonymous",
+  password: "anonymous"
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+const FtpSrv = require('ftp-srv');
+
+const hostname = '0.0.0.0';
+const port = 1111
+
+const ftpServer = new FtpSrv('ftp://' + hostname + ':' + port,
+  { anonymous: true, greeting: ["Hello Jong", "Wie gehts?"] });
+
+ftpServer.on('login', ({ connection, username, password }, resolve, reject) => {
+  if (username === 'anonymous' && password === 'anonymous') {
+
+    connection.on('command', (command, callback) => {
+      console.log(command)
+      callback(null, '200 OK', 'Hello ' + username);
+    }
+    )
+    connection.on('STOR', (error, fileName) => {
+      if (error) {
+        console.error(`FTP server error: could not receive file ${fileName} for upload ${error}`);
+      }
+      console.info(`FTP server: upload successfully received - ${fileName}`);
+    });
+    resolve();
+  }
+  return reject(new errors.GeneralError('Invalid username or password', 401));
+  console.log('resolve: ' + resolve);
+  console.log('reject: ' + reject);
+
+});
+
+ftpServer.on('client-error', (connection, context, error) => {
+  console.log('connection: ' + connection);
+  console.log('context: ' + context);
+  console.log('error: ' + error);
+});
+
+
+
+
+ftpServer.listen()
+  .then(() => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
